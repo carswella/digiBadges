16ead11c18769ed824a6c9ed2a756ad39221bfbb
carswella <carswella@hillmorton.school.nz>
2016-11-11 16:50:07 +1300

    Add files via upload

---
+++ hmmmm.py
@@ -0,0 +1,19 @@
+'''
+Created on 11/11/2016
+
+@author: carswella
+'''
+
+import os
+#import PIL
+print dir(os)
+#print dir(PIL)
+print 'hello'
+
+d = {}
+d['aaa'] = 5
+d['bbb'] = 6
+d['ccc'] = 7
+
+for item in d.items():
+    print item
